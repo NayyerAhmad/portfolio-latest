e3be759c4e7b571df8f56da32dbb70222e3d1358
Nayyer Ahmad <nayyer.amd@gmail.com>
2024-07-27 21:46:58 +0500

    typescript error within footer fixed

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -12,41 +12,37 @@ const RecentProjects = () => {
         </h1>
         <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
             {projects.map(({id, title, des, img, iconLists, link }) => (
-                <a 
-                  key={id} 
-                  href={`https://${link}`} // Ensure link is formatted as absolute URL
-                  target="_blank" 
-                  rel="noopener noreferrer" 
-                  className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'
-                >
-                    <PinContainer title={link} href={link}>
-                        <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
-                            <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
-                                <img src="/bg.png" alt="bg-img" />
+                <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
+                    <a href={link} target="_blank" rel="noopener noreferrer" className='block'>
+                        <PinContainer title={link}>
+                            <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
+                                <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
+                                    <img src="/bg.png" alt="bg-img" />
+                                </div>
+                                <img src={img} alt={title} className='z-10 absolute bottom-0' />
                             </div>
-                            <img src={img} alt={title} className='z-10 absolute bottom-0' />
-                        </div>
-                        <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
-                            {title}
-                        </h1>
-                        <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
-                            {des}
-                        </p>
-                        <div className='flex items-center justify-between mt-7 mb-3'>
-                            <div className='flex items-center'>
-                                {iconLists.map((icon, index) => (
-                                    <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center' style={{transform: `translateX(-${5 * index * 2}px)`}}>
-                                        <img src={icon} alt={icon} className='p-2'/>
-                                    </div>
-                                ))}
+                            <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+                                {title}
+                            </h1>
+                            <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+                                {des}
+                            </p>
+                            <div className='flex items-center justify-between mt-7 mb-3'>
+                                <div className='flex items-center'>
+                                    {iconLists.map((icon, index) => (
+                                        <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center' style={{transform: `translateX(-${5 * index * 2}px)`}}>
+                                            <img src={icon} alt={icon} className='p-2'/>
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className='flex justify-center items-center'>
+                                    <p className='flex lg:text-xl md:text-xs text-sm text-purple'>Check Live Site</p>
+                                    <FaLocationArrow className='ms-3' color='#CBACF9'/>
+                                </div>
                             </div>
-                            <div className='flex justify-center items-center'>
-                                <p className='flex lg:text-xl md:text-xs text-sm text-purple'>Check Live Site</p>
-                                <FaLocationArrow className='ms-3' color='#CBACF9'/>
-                            </div>
-                        </div>
-                    </PinContainer>
-                </a>
+                        </PinContainer>
+                    </a>
+                </div>
             ))}
         </div>
     </div>
