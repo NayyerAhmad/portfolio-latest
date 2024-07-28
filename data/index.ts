ab7a9a6e78f891cbe383c7dbcfd7105d5cdcd7b7
Nayyer Ahmad <nayyer.amd@gmail.com>
2024-07-28 13:13:21 +0500

    links for footer updated

--- data/index.ts
+++ data/index.ts
@@ -171,13 +171,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/NayyerAhmad"
   },
   {
     id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/nayyer-ahmad/"
   },
 ];
